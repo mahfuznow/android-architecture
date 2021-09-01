36be63494904e2361a94e07860865312c008136f
Mahfuzur Rahman <mahfuz.cse.just@gmail.com>
2021-09-01 09:27:26 +0600

    Fixed library Changed "retrofit2:adapter-rxjava2" to "retrofit2:adapter-rxjava3"

--- app/src/main/java/com/mahfuznow/androidarchitecture/model/CountryService.java
+++ app/src/main/java/com/mahfuznow/androidarchitecture/model/CountryService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CountryService implements CountryAPI{
@@ -16,7 +16,7 @@ public class CountryService implements CountryAPI{
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                 .build();
         api = retrofit.create(CountryAPI.class);
     }
